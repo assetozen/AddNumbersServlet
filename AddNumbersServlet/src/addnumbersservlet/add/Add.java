91a5f3035dcd6198570aa758e1031806297dfcfc
assetozen <assetozen@yahoo.com>
2019-03-05 16:04:11 +0100

    Init Commit

---
+++ AddNumbersServlet/src/addnumbersservlet/add/Add.java
@@ -0,0 +1,36 @@
+package addnumbersservlet.add;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/Add")
+public class Add extends HttpServlet {
+
+	/**
+	 * Default serial number
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		processRequest(request, response);
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		processRequest(request, response);
+	}
+
+	public void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		int i = Integer.parseInt(request.getParameter("num1"));
+		int j = Integer.parseInt(request.getParameter("num2"));
+
+		int k = i + j;
+		PrintWriter out = response.getWriter();
+		out.println(k);
+	}
+
+}
